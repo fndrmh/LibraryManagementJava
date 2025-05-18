95f7c70a6711a0f30c911c09a2adcbf017b10989
mh <mmdhosseinvd@gmail.com>
2025-05-18 22:37:02 +0330

    student getter and setter is ready

--- src/model/Student.java
+++ src/model/Student.java
@@ -5,12 +5,22 @@ public abstract class Student {
   protected  String firstName;
   protected  String lastName;
   protected  String major;
-public Student(String studentId,String firstName,String lastName,String major)
-{
+public Student(String studentId,String firstName,String lastName,String major){
       this.studentId=studentId;
       this.firstName=firstName;
       this.lastName=lastName;
       this.major=major;
 }
   public abstract void getStudentDetails();
+//Setter
+  public void setStudentId (String studentId) {this.studentId=studentId;}
+  public void setFirstName (String firstName) {this.firstName=firstName;}
+  public void setLastName  (String lastName)  {this.lastName=lastName;}
+  public void setMajor     (String major)     {this.major=major;}
+//Getter
+  public String getStudentId() {return this.studentId;}
+  public String getFirstName() {return this.firstName;}
+  public String getLastName()  {return this.lastName;}
+  public String getMajor()     {return this.major;}
+
 }
