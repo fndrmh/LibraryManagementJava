5a86344ff6fb9d507ac05944d862beb035551454
mh <mmdhosseinvd@gmail.com>
2025-05-19 14:26:00 +0330

    GraduateStudent finished

--- src/model/GraduateStudent.java
+++ src/model/GraduateStudent.java
@@ -8,10 +8,16 @@ public class GraduateStudent extends Student {
     // TODO: Implement method 'getStudentDetails' for GraduateStudent.
     throw new UnsupportedOperationException("Unimplemented method 'getStudentDetails'");
   }
-public GraduateStudent(String studentId,String firstName,String lastName,String major,String superString,String theString)
-{
+public GraduateStudent(String studentId,String firstName,String lastName,String major,String superString,String theString){
       super(studentId, firstName, lastName, major);
       this.supervisor=supervisor;
       this.thesisTitle=thesisTitle;
 }
+//Getter
+public String getSupervisor()  {return supervisor;}
+public String getThesisTitle() {return thesisTitle;}
+//Setter
+public void setSupervisor (String supervisor)  {this.supervisor=supervisor;}
+public void setThesisTitle(String thesisTitle) {this.thesisTitle=thesisTitle;}
 }
+
