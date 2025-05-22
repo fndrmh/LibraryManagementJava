ef4e9d8fd48b301662690f13b90b57c1b835292a
Reza <reza8n1@gmail.com>
2025-05-22 22:45:56 +0330

    add setStudents & reformat

--- src/model/Library.java
+++ src/model/Library.java
@@ -1,4 +1,5 @@
 package model;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -9,149 +10,158 @@ public class Library {
   private List<Student> students;
   private List<Category> categories;
   private List<Loan> loans;
-//Constructor
-  public Library(){
-      books=new ArrayList<>();
-      students=new ArrayList<>();
-      categories=new ArrayList<>();
-      loans=new ArrayList<>();
+
+  // Constructor
+  public Library() {
+    books = new ArrayList<>();
+    students = new ArrayList<>();
+    categories = new ArrayList<>();
+    loans = new ArrayList<>();
   }
-  public void setBooks(List<Book> books){
-      this.books=books;
+
+  public void setBooks(List<Book> books) {
+    this.books = books;
   }
-  public List<Book> getBooks(){
-      return this.books;
+
+  public List<Book> getBooks() {
+    return this.books;
   }
+
   public void addBook(Book book) {
-      books.add(book);
+    books.add(book);
   }
 
   public void removeBook(String isbn) {
-      for (Book book : books) {
-            if(book.getIsbn().equals(isbn)){
-                  books.remove(book);
-            }
+    for (Book book : books) {
+      if (book.getIsbn().equals(isbn)) {
+        books.remove(book);
       }
+    }
   }
 
   public Book searchBookByTitle(String title) {
-      for(Book book : books){
-            if(book.getTitle().equals(title)){
-                  return book;
-            }
+    for (Book book : books) {
+      if (book.getTitle().equals(title)) {
+        return book;
       }
-      return null;
+    }
+    return null;
   }
 
   public Book searchBookByISBN(String isbn) {
-            for(Book book : books){
-            if(book.getIsbn().equals(isbn)){
-                  return book;
-            }
+    for (Book book : books) {
+      if (book.getIsbn().equals(isbn)) {
+        return book;
       }
-      return null;
+    }
+    return null;
   }
 
   public List<Book> getAvailableBooks() {
-      List<Book> AvailableBooks=new ArrayList<>();
-      for(Book book : books){
-            if(!book.getIsBorrowed()){
-                  AvailableBooks.add(book);
-            }
+    List<Book> AvailableBooks = new ArrayList<>();
+    for (Book book : books) {
+      if (!book.getIsBorrowed()) {
+        AvailableBooks.add(book);
       }
-      return AvailableBooks;
+    }
+    return AvailableBooks;
+  }
+
+  public void setStudents(List<Student> students) {
+    this.students = students;
   }
 
   public List<Student> getAllStudents() {
-      return students;
+    return students;
   }
+
   public void addStudent(Student student) {
-      students.add(student);
+    students.add(student);
   }
 
   public void removeStudent(String studentId) {
-      for(Student student : students){
-            if(student.getStudentId().equals(studentId)){
-                  students.remove(student);
-            }
+    for (Student student : students) {
+      if (student.getStudentId().equals(studentId)) {
+        students.remove(student);
       }
+    }
   }
 
   public Student searchStudentById(String studentId) {
-      for(Student student : students){
-            if(student.getStudentId().equals(studentId)){
-                  return student;
-            }
+    for (Student student : students) {
+      if (student.getStudentId().equals(studentId)) {
+        return student;
       }
-      return null;
+    }
+    return null;
   }
-  public void setLoans(List<Loan> loans){
-      this.loans=loans;
+
+  public void setLoans(List<Loan> loans) {
+    this.loans = loans;
   }
-  public List<Loan> getLoans(){
-      return this.loans;
+
+  public List<Loan> getLoans() {
+    return this.loans;
   }
+
   public boolean borrowBook(Book book, Student student) {
-      if(book.getIsBorrowed()){
-            return false;
-      }
+    if (book.getIsBorrowed()) {
+      return false;
+    }
 
-      Date loanDate=new Date();
-      Calendar calendar= Calendar.getInstance();
-      calendar.setTime(loanDate);
-      calendar.add(Calendar.DAY_OF_MONTH,10);
-      Date dueDate = calendar.getTime();
-      book.borrowBook();
-      Loan loan=new Loan(book,student,loanDate,dueDate);
-      loans.add(loan);
-      return true;
+    Date loanDate = new Date();
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(loanDate);
+    calendar.add(Calendar.DAY_OF_MONTH, 10);
+    Date dueDate = calendar.getTime();
+    book.borrowBook();
+    Loan loan = new Loan(book, student, loanDate, dueDate);
+    loans.add(loan);
+    return true;
   }
-
 
   public boolean returnBook(Book book) {
-      for(Loan loan :loans){
-            if(loan.getBook().equals(book)){
-                  loans.remove(loan);
-                  loan.getBook().returnBook();
-                  return true;
-            }
+    for (Loan loan : loans) {
+      if (loan.getBook().equals(book)) {
+        loans.remove(loan);
+        loan.getBook().returnBook();
+        return true;
       }
-      return false;
+    }
+    return false;
   }
 
   public List<Book> getBorrowedBooksByStudent(String studentId) {
-      List<Book> borrowedBooks=new ArrayList<>();
-      for(Loan loan : loans){
-            if(loan.getStudent().studentId.equals(studentId)){
-                  borrowedBooks.add(loan.getBook());
-            }
+    List<Book> borrowedBooks = new ArrayList<>();
+    for (Loan loan : loans) {
+      if (loan.getStudent().studentId.equals(studentId)) {
+        borrowedBooks.add(loan.getBook());
       }
-      return borrowedBooks;
+    }
+    return borrowedBooks;
   }
-  public void setCategories(List<Category> categorios){
-      this.categories=categorios;
-  }
-  public List<Category> getCategories(){
-      return this.categories;
-  }
-  public List<Book> getBooksByCategory(String categoryName) {
-      /*for(Category category : categories){
-            if(category.getName().equals(categoryName)){
 
-            }
-      } */
-         // TODO: Implement method 'getStudentsByMajor'.
-    throw new UnsupportedOperationException("Unimplemented method 'getStudentsByMajor'");
+  public void setCategories(List<Category> categorios) {
+    this.categories = categorios;
+  }
+
+  public List<Category> getCategories() {
+    return this.categories;
+  }
+
+  public List<Book> getBooksByCategory(String categoryName) {
+    // TODO: Implement method 'getBooksByCategory'.
+    throw new UnsupportedOperationException("Unimplemented method 'getBooksByCategory'");
 
   }
 
   public List<Student> getStudentsByMajor(String major) {
-      List<Student> studentsByMajor=new ArrayList<>();
-      for(Student student :students){
-            if(student.getMajor().equals(major)){
-                  studentsByMajor.add(student);
-            }
+    List<Student> studentsByMajor = new ArrayList<>();
+    for (Student student : students) {
+      if (student.getMajor().equals(major)) {
+        studentsByMajor.add(student);
       }
-      return studentsByMajor;
+    }
+    return studentsByMajor;
   }
 }
