79b9960abf987dc8714e84f7760c36a9a2eef414
Reza <reza8n1@gmail.com>
2025-05-22 19:52:32 +0330

    Don't get filePath in fileDataModel's methods

--- src/data/FileDataModel.java
+++ src/data/FileDataModel.java
@@ -4,42 +4,47 @@ import java.util.List;
 import model.*;
 
 public class FileDataModel {
-  public void loadBooks(String filePath) {
+  private static final String BOOKS_FILEPATH = "books.json";
+  private static final String STUDENTS_FILEPATH  = "students.json";
+  private static final String CATEGORIES_FILEPATH = "categories.json";
+  private static final String LOANS_FILEPATH = "loans.json";
+
+  public void loadBooks() {
     // TODO: Implement method 'filePath'.
     throw new UnsupportedOperationException("Unimplemented method 'filePath'");
   }
 
-  public void saveBooks(List<Book> books, String filePath) {
+  public void saveBooks(List<Book> books) {
     // TODO: Implement method 'saveBooks'.
     throw new UnsupportedOperationException("Unimplemented method 'saveBooks'");
   }
 
-  public void loadStudents(String filePath) {
+  public void loadStudents() {
     // TODO: Implement method 'loadStudents'.
     throw new UnsupportedOperationException("Unimplemented method 'loadStudents'");
   }
 
-  public void saveStudents(List<Student> students, String filePath) {
+  public void saveStudents(List<Student> students) {
     // TODO: Implement method 'saveStudents'.
     throw new UnsupportedOperationException("Unimplemented method 'saveStudents'");
   }
 
-  public void loadCategories(String filePath) {
+  public void loadCategories() {
     // TODO: Implement method 'loadCategories'.
     throw new UnsupportedOperationException("Unimplemented method 'loadCategories'");
   }
 
-  public void saveCategories(List<Category> categories, String filePath) {
+  public void saveCategories(List<Category> categories) {
     // TODO: Implement method 'saveCategories'.
     throw new UnsupportedOperationException("Unimplemented method 'saveCategories'");
-  }
+  } 
 
-  public void loadLoans(String filePath) {
+  public void loadLoans() {
     // TODO: Implement method 'loadLoans'.
     throw new UnsupportedOperationException("Unimplemented method 'loadLoans'");
   }
 
-  public void saveLoans(List<Loan> loans, String filePath) {
+  public void saveLoans(List<Loan> loans) {
     // TODO: Implement method 'saveLoans'.
     throw new UnsupportedOperationException("Unimplemented method 'saveLoans'");
   }
